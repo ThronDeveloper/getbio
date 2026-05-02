451cabb404964a2c3f4a3902d6168b7e4f869f0b
Umut Duz <umutduz5454@gmail.com>
2026-05-02 15:38:46 +0300

    Update page.tsx

--- src/app/git/page.tsx
+++ src/app/git/page.tsx
@@ -1,6 +1,8 @@
 import { getProfileData, type ProfileData, type GitRepo } from "@/lib/data";
 import GitClient from "./git-client";
 
+export const dynamic = 'force-dynamic';
+
 export default async function GitPage() {
     const data = await getProfileData();
     return <GitClient data={data} />;
